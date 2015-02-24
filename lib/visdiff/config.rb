000b7856f3d616530eaff5c7aff284e42f4f73f8
John Hawthorn <john.hawthorn@gmail.com>
2015-02-23 22:56:17 -0800

    Add debug flag to turn on faraday logging

--- lib/visdiff/config.rb
+++ lib/visdiff/config.rb
@@ -2,7 +2,7 @@ require 'faraday'
 
 module Visdiff
   class Config
-    attr_accessor :base_url, :api_key
+    attr_accessor :base_url, :api_key, :debug
 
     def initialize
     end
@@ -12,6 +12,7 @@ module Visdiff
       @connection ||= Faraday.new(:url => base_url) do |faraday|
         faraday.request  :multipart
         faraday.request  :url_encoded
+        faraday.response :logger if debug
         faraday.adapter  Faraday.default_adapter
       end
     end
